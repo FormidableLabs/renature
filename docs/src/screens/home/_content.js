fd9761fa1b74c31e1952f9697d43c963077aa784
Parker Ziegler <parkerziegler@users.noreply.github.com>
2020-01-12 15:55:16 -0800

    Add docs folder and begin reskinning spectacle docs for renature. (#21)

---
+++ docs/src/screens/home/_content.js
@@ -0,0 +1,72 @@
+/* eslint-disable filenames/match-regex */
+/* eslint-disable quotes*/
+const content = {
+  features: [
+    {
+      title: 'Declarative React hooks for animating with ease',
+      description:
+        'Tweak your physics parameters, set from and to values for your CSS properties, and let renature do the rest.',
+      icon: require('../../static/svgs/button.svg'),
+    },
+    {
+      title: 'Gravity, Friction, Fluid Resistance, and more',
+      description:
+        'Renature explores forces that other physics-based animation libraries typically leave out, giving your animations unique feeling and intuitive motion.',
+      icon: require('../../static/svgs/code-preview.svg'),
+    },
+    {
+      title: 'An animation library for physics nerds',
+      description:
+        'Renature emphasizes mathematical precision and correctness, all backed by the type safety and speed of ReasonML.',
+      icon: require('../../static/svgs/amazing.svg'),
+    },
+  ],
+  preview: {
+    description: '',
+    media: '',
+  },
+  getStarted: {
+    description:
+      'Renature comes equipped with a lightweight set of production ready React hooks. Dig into the documentation to start animating!',
+    link: '/docs',
+  },
+  oss: [
+    {
+      title: 'Victory',
+      description:
+        'An ecosystem of modular data visualization components for React. Friendly and flexible.',
+      logo: require('../../static/svgs/logo_victory.svg'),
+      link: 'https://formidable.com/open-source/victory',
+      hasOwnLogo: true,
+    },
+    {
+      title: 'Development Dashboards',
+      description:
+        'Dashboards to organize and intuitively display your dev server and tooling output.',
+      abbreviation: 'Dd',
+      color: '#8bd48b',
+      number: '17',
+      link: 'https://formidable.com/open-source/development-dashboards/',
+    },
+    {
+      title: 'React Animations',
+      description:
+        'A collection of animations that can be used with many inline style libraries, such as Radium or Aphrodite.',
+      abbreviation: 'Ra',
+      color: '#86b9e6',
+      number: '03',
+      link: 'https://formidable.com/open-source/react-animations',
+    },
+    {
+      title: 'Enzyme Matchers',
+      description:
+        'Run common assertions on your React components using Enzyme in a Jest or Jasmine environment.',
+      abbreviation: 'Em',
+      color: '#e48055',
+      number: '09',
+      link: 'https://formidable.com/open-source/jest-enzyme/',
+    },
+  ],
+};
+
+export default content;
