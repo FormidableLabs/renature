5a5eb8c47d8e0a1502bb8328f54c60c52e78355a
parkerziegler <parker.ziegler@formidable.com>
2020-01-24 15:58:51 -0800

    Add support for react-live as our previewing element.

--- docs/src/screens/home/preview.js
+++ docs/src/screens/home/preview.js
@@ -1,10 +1,108 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
+import * as renature from 'renature';
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live';
 
 import { Wrapper } from '../../components/wrapper';
 import { SectionTitle } from '../../components/section-title';
 import { theme } from '../../theme';
 import { BodyCopy } from '../../components/body-copy';
+import { prismTheme } from './prism-theme';
+
+const StyledProvider = styled(LiveProvider)`
+  border-radius: 0.5rem;
+  box-shadow: 1px 1px 20px rgba(20, 20, 20, 0.27);
+  overflow: hidden;
+  margin-bottom: 1rem;
+`;
+
+const column = css`
+  flex-basis: 50%;
+  width: 50%;
+  max-width: 50%;
+  @media (max-width: 600px) {
+    flex-basis: auto;
+    width: 100%;
+    max-width: 100%;
+  }
+`;
+
+const StyledEditor = styled(LiveEditor)`
+  margin-top: 4rem;
+  border-top-left-radius: 1rem;
+  border-bottom-left-radius: 1rem;
+  font-family: 'Source Code Pro', monospace;
+  font-size: 1.4rem;
+  height: 35rem;
+  max-height: 35rem;
+  overflow: auto;
+  ${column};
+
+  * > textarea:focus {
+    outline: none;
+  }
+`;
+
+const StyledPreview = styled(LivePreview)`
+  margin-top: 4rem;
+  border-top-right-radius: 1rem;
+  border-bottom-right-radius: 1rem;
+  position: relative;
+  padding: 0.5rem;
+  background: white;
+  color: black;
+  height: auto;
+  overflow: hidden;
+  ${column};
+
+  .mover {
+    position: relative;
+    top: 50%;
+    left: 50%;
+    height: 100px;
+    width: 100px;
+    transform: translate(-50%, -50%);
+    border-radius: 10px;
+    transform-origin: top left;
+
+    &--opacity {
+      background: #a04ad9;
+    }
+  }
+`;
+
+const StyledError = styled(LiveError)`
+  display: block;
+  padding: 8rem;
+  background: red;
+  color: white;
+  white-space: pre-wrap;
+  text-align: left;
+  font-size: 0.9em;
+  font-family: 'Source Code Pro', monospace;
+`;
+
+const scope = { renature };
+const code = `
+function FrictionAnimation() {
+  const [props] = renature.useFriction({
+    from: {
+      transform: 'translateX(-100px) translate(-50%, -50%)'
+    },
+    to: {
+      transform: 'translateX(100px) translate(-50%, -50%)'
+    },
+    config: {
+      mu: 0.5,
+      mass: 300,
+      initialVelocity: 10
+    },
+    infinite: true
+  });
+
+  return <div className="mover mover--opacity" {...props} />;
+}
+`;
 
 const Preview = () => (
   <Wrapper background={theme.colors.backgroundGradient}>
@@ -14,6 +112,11 @@ const Preview = () => (
     <BodyCopy color={theme.colors.textLight}>
       Renature is all about bringing joy and whimsy to UI animation.
     </BodyCopy>
+    <StyledProvider code={code} scope={scope} theme={prismTheme}>
+      <StyledEditor />
+      <StyledError />
+      <StyledPreview />
+    </StyledProvider>
   </Wrapper>
 );
 
---
+++ docs/src/screens/home/prism-theme.js
@@ -0,0 +1,86 @@
+export const prismTheme = {
+  plain: {
+    color: '#d6deeb',
+    backgroundColor: '#011627',
+  },
+  styles: [
+    {
+      types: ['changed'],
+      style: {
+        color: 'rgb(162, 191, 252)',
+      },
+    },
+    {
+      types: ['deleted'],
+      style: {
+        color: 'rgba(239, 83, 80, 0.56)',
+      },
+    },
+    {
+      types: ['inserted', 'string', 'attr-name', 'url'],
+      style: {
+        color: 'rgb(173, 219, 103)',
+      },
+    },
+    {
+      types: ['comment'],
+      style: {
+        color: 'rgb(99, 119, 119)',
+      },
+    },
+    {
+      types: ['variable'],
+      style: {
+        color: 'rgb(214, 222, 235)',
+      },
+    },
+    {
+      types: ['number'],
+      style: {
+        color: 'rgb(247, 140, 108)',
+      },
+    },
+    {
+      types: ['builtin', 'char', 'constant'],
+      style: {
+        color: 'rgb(130, 170, 255)',
+      },
+    },
+    {
+      types: ['punctuation', 'function', 'keyword', 'selector', 'doctype'],
+      style: {
+        color: 'rgb(199, 146, 234)',
+      },
+    },
+    {
+      types: ['class-name'],
+      style: {
+        color: 'rgb(255, 203, 139)',
+      },
+    },
+    {
+      types: ['tag', 'operator'],
+      style: {
+        color: 'rgb(127, 219, 202)',
+      },
+    },
+    {
+      types: ['boolean'],
+      style: {
+        color: 'rgb(255, 88, 116)',
+      },
+    },
+    {
+      types: ['property'],
+      style: {
+        color: 'rgb(128, 203, 196)',
+      },
+    },
+    {
+      types: ['namespace'],
+      style: {
+        color: 'rgb(178, 204, 214)',
+      },
+    },
+  ],
+};
