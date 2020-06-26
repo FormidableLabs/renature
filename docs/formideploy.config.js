b129e6dd891da0a81c49c3fb8b4cce555c56fc21
Ryan Roemer <ryan.roemer@formidable.com>
2020-06-26 10:43:59 -0700

    Get everything set up for formideploy base branch.

---
+++ docs/formideploy.config.js
@@ -0,0 +1,13 @@
+/**
+ * Formideploy configuration overrides.
+ */
+module.exports = {
+  lander: {
+    name: 'renature'
+  },
+  production: {
+    // TODO: REMOVE
+    domain: 'tmp-experiment-02.formidable.com',
+    bucket: 'tmp-experiment-02.formidable.com'
+  }
+};
