25e5c0e99d2a984006d9a57d358fd0321acefa2b
Ryan Roemer <ryan.roemer@formidable.com>
2021-02-04 06:59:42 -0800

    Prep temp domain

--- docs/formideploy.config.js
+++ docs/formideploy.config.js
@@ -3,6 +3,12 @@
  */
 module.exports = {
   lander: {
-    name: 'renature'
-  }
+    name: 'renature',
+  },
+  // TODO(GH): Remove this after we've fully dog-fooded our switch to GH actions.
+  production: {
+    domain:
+      'tmp-experiment-02.formidable.com.s3-website-us-east-1.amazonaws.com',
+    bucket: 'tmp-experiment-02.formidable.com',
+  },
 };
