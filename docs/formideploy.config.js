d261dc33f9f7e268d1b82346b8447adc2e29506e
Ryan Roemer <ryan.roemer@formidable.com>
2021-02-05 03:55:52 -0800

    CI/Docs: Fix production publishing. (#121)
    - Bump to published formideploy with region support. Fixes #118 
    - Prepare for real production.

--- docs/formideploy.config.js
+++ docs/formideploy.config.js
@@ -5,9 +5,4 @@ module.exports = {
   lander: {
     name: 'renature',
   },
-  // TODO(GH): Remove this after we've fully dog-fooded our switch to GH actions.
-  production: {
-    domain: 'tmp-experiment-02.formidable.com',
-    bucket: 'tmp-experiment-02.formidable.com',
-  },
 };
